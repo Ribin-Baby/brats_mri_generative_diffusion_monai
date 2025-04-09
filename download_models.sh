92fa3631aa95641d1e2773c95d3ac961113cd44d
Ribin-Baby <ribinkannoth@gmail.com>
2025-04-09 17:03:38 +0530

    execute to download models

---
+++ download_models.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Create /models directory if it doesn't exist
+MODEL_DIR="/models"
+mkdir -p "$MODEL_DIR"
+
+# Define URLs
+URLS=(
+  "https://github.com/Ribin-Baby/brats_mri_generative_diffusion_monai/releases/download/Monai-models/model.pt"
+  "https://github.com/Ribin-Baby/brats_mri_generative_diffusion_monai/releases/download/Monai-models/model_autoencoder.pt"
+  "https://github.com/Ribin-Baby/brats_mri_generative_diffusion_monai/releases/download/Monai-models/best_metric_model.pth"
+)
+
+# Download each file into /models
+echo "Downloading files into $MODEL_DIR ..."
+for url in "${URLS[@]}"; do
+    echo "Downloading: $url"
+    wget -q --show-progress -P "$MODEL_DIR" "$url"
+done
+
+echo "Download completed!"
